fed3f7e0b2dfff70a8e295b1061a3c052dca0d44
Darren Gallagher <darren.gallagher@satavia.com>
2020-06-15 03:01:37 +0100

    Added create_modulefile for WPS

--- apps/wrf/install_wps_openmpi.sh
+++ apps/wrf/install_wps_openmpi.sh
@@ -4,9 +4,24 @@ APP_NAME=wps
 APP_VERSION=4.1
 SKU_TYPE=${SKU_TYPE:-hb}
 SHARED_APP=${SHARED_APP:-/apps}
+MODULE_DIR=${SHARED_APP}/modulefiles/${SKU_TYPE}/${APP_NAME}
+MODULE_NAME=${APP_VERSION}-openmpi
 WRF_VERSION=${WRF_VERSION:-4.1.5}
 APP_DIR=${SHARED_APP}/${SKU_TYPE}/${APP_NAME}-openmpi
 OPENMPI_VER=4.0.3
+
+function create_modulefile {
+mkdir -p ${MODULE_DIR}
+cat << EOF >> ${MODULE_DIR}/${MODULE_NAME}
+#%Module
+set              wpsversion        ${APP_VERSION}
+set              WPSROOT           ${APP_DIR}/WPS-\$wpsversion
+setenv           WPSROOT           ${APP_DIR}/WPS-\$wpsversion
+
+append-path      PATH              \$WPSROOT
+append-path      PATH              \$WPSROOT/util
+EOF
+}
 
 sudo yum install -y jasper-devel
 sudo yum install -y libpng-devel
