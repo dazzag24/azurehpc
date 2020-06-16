2e9d8ead5c0694a5ba5ab7d1353233d919da36f8
dazzag24 <dazzag@gmail.com>
2020-06-16 14:57:35 +0100

    Fix to address https://github.com/Azure/azurehpc/issues/294
    Something is incorrectly setting MPI_LIB which results in metgrid and geogrid not compiling.

--- apps/wrf/install_wps_openmpi.sh
+++ apps/wrf/install_wps_openmpi.sh
@@ -5,6 +5,7 @@ APP_VERSION=4.1
 SKU_TYPE=${SKU_TYPE:-hb}
 SHARED_APP=${SHARED_APP:-/apps}
 WRF_VERSION=4.1.3
+OPENMPI_VER=4.0.3
 APP_DIR=${SHARED_APP}/${SKU_TYPE}/${APP_NAME}-openmpi
 
 sudo yum install -y jasper-devel
@@ -27,6 +28,7 @@ module load gcc-9.2.0
 export HDF5=$(spack location -i hdf5^openmpi)
 export NETCDF=$(spack location -i netcdf-fortran^openmpi)
 export WRF_DIR=${SHARED_APP}/${SKU_TYPE}/wrf-openmp/WRF-${WRF_VERSION}
+export MPI_LIB="-L/openmpi-${OPENMPI_VER}/lib -lmpi"
 
 cd WPS-${APP_VERSION}
 ./configure << EOF
